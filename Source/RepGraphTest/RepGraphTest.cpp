2760c08465de5bd9be6795d3b531cbe8f522a0fb
Markus Ort <markus@gameplayuniver.se>
2020-07-24 15:23:14 +0200

    Added UProject

---
+++ Source/RepGraphTest/RepGraphTest.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RepGraphTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RepGraphTest, "RepGraphTest" );
---
+++ Source/RepGraphTest/TestRepGraph.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ReplicationGraph.h"
+#include "TestRepGraph.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REPGRAPHTEST_API UTestRepGraph : public UReplicationGraph
+{
+	GENERATED_BODY()
+	
+};
