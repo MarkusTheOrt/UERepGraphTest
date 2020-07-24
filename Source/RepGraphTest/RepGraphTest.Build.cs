c8d7b21d17a3920a9cd0e12a5cee070692ec559f
Markus Ort <markus@gameplayuniver.se>
2020-07-24 15:28:10 +0200

    Init Git LFS

--- Source/RepGraphTest/RepGraphTest.Build.cs
+++ Source/RepGraphTest/RepGraphTest.Build.cs
@@ -8,16 +8,24 @@ public class RepGraphTest : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
-
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core", 
+				"CoreUObject", 
+				"Engine", 
+				"InputCore",
+				"OnlineSubsystemUtils",
+				"OnlineSubsystemNull"
+			});
 		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Slate", 
+				"SlateCore",
+				"OnlineSubsystem"
+			});
 	}
 }
